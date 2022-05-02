3fc77b0869aa3f25a7114c430f9c0250bfbaf992
MadsOertoft <mads@oconsoft.com>
2022-05-02 12:52:54 +0200

    Meter and Measurements controllers added api-endpoints

--- Teknologi projekt 2022 energistyring MVC/Controllers/MeasurementsController.cs
+++ Teknologi projekt 2022 energistyring MVC/Controllers/MeasurementsController.cs
@@ -7,9 +7,12 @@ namespace Teknologi_projekt_2022_energistyring_MVC.Controllers {
 
     public class MeasurementsController : ControllerBase {
 
+        /********************************************************************************
+        *                                HTTP GET
+        * ******************************************************************************/
         [Route("api/[controller]")]
         [HttpGet]
-        public async Task<IResult> Get([FromServices]IMeasurementData data) {
+        public async Task<IResult> GetMeasurements([FromServices]IMeasurementData data) {
             try {
                 return Results.Ok(await data.GetMeasurements());
             }
@@ -18,9 +21,45 @@ namespace Teknologi_projekt_2022_energistyring_MVC.Controllers {
             }
         }
 
+        [Route("api/[controller]/GetInterval")]
+        [HttpGet]
+        public async Task<IResult> GetMeasurementsByInterval(DateTime start, DateTime end, [FromServices] IMeasurementData data) {
+            try {
+                return Results.Ok(await data.GetMeasurementsByTime(start, end));
+            }
+            catch (Exception ex) {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        [Route("api/[controller]")]
+        [HttpGet]
+        public async Task<IResult> GetMeasurementsByMeterId(string MeterId, [FromServices] IMeasurementData data) {
+            try {
+                return Results.Ok(await data.GetMeasurementsByMeterId(MeterId));
+            }
+            catch (Exception ex) {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        [Route("api/[controller]")]
+        [HttpGet]
+        public async Task<IResult> GetMeasurementById(int Id, [FromServices] IMeasurementData data) {
+            try {
+                return Results.Ok(await data.GetMeasurement(Id));
+            }
+            catch (Exception ex) {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        /********************************************************************************
+        *                                HTTP POST
+        * ******************************************************************************/
         [Route("api/[controller]")]
         [HttpPost]
-        public async Task<IResult> Post([FromBody]MeasurementModel measurement, [FromServices] IMeasurementData data) {
+        public async Task<IResult> InserMeasurement([FromBody]MeasurementModel measurement, [FromServices] IMeasurementData data) {
             try {
                 await data.InsertMeasurement(measurement);
                 return Results.Ok();
@@ -31,16 +70,22 @@ namespace Teknologi_projekt_2022_energistyring_MVC.Controllers {
         }
 
 
-        [Route("api/[controller]/GetInterval")]
-        [HttpGet]
-        public async Task<IResult> GetInterval(DateTime start, DateTime end, [FromServices] IMeasurementData data) {
+        /********************************************************************************
+        *                                HTTP DELETE
+        * ******************************************************************************/
+
+        [Route("api/[controller]")]
+        [HttpDelete]
+        public async Task<IResult> DeleteMeasurementById(int Id, [FromServices] IMeasurementData data) {
             try {
-                return Results.Ok(await data.GetMeasurementsByTime(start, end));
+                await data.DeleteMeasurement(Id);
+                return Results.Ok();
             }
             catch (Exception ex) {
                 return Results.Problem(ex.Message);
             }
         }
+
 
     }
 }
---
+++ Teknologi projekt 2022 energistyring MVC/Controllers/MeterController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Teknologi_projekt_2022_energistyring_MVC.Controllers {
+    [ApiController]
+    public class MeterController : ControllerBase {
+        
+        /********************************************************************************
+        *                                HTTP GET
+        * ******************************************************************************/
+        [Route("api/[controller]")]
+        [HttpGet]
+        public async Task<IResult> Get([FromServices] IMeasurementData data) {
+            try {
+                return Results.Ok(await data.GetMeters());
+            }
+            catch (Exception ex) {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+
+        /********************************************************************************
+        *                                HTTP DELETE
+        * ******************************************************************************/
+        [Route("api/[controller]")]
+        [HttpDelete]
+        public async Task<IResult> DeleteMeasurementByMeterId(string MeterId, [FromServices] IMeasurementData data) {
+            try {
+                await data.DeleteMeasurementsByMeterId(MeterId);
+                return Results.Ok();
+            }
+            catch (Exception ex) {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+
+
+    }
+}
